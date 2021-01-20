313007080193e930247a1e74f74cd53d5225093a
Calixd7 <calixd7@gmail.com>
2021-01-20 09:36:52 -0500

    models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from core.models import User, Habit, Record
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Habit)
+admin.site.register(Record)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import UniqueConstraint
+
 
 class User(AbstractUser):
     pass
+
+class Habit (models.Model):
+    user = models.ForeignKey(User, related_name="habits", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    target = models.IntegerField(blank=True, null=True)
+    date = models.DateField(auto_now=True)
+
+
+
+class Record (models.Model):
+    number = models.IntegerField()
+    habit = models.ForeignKey('Habit', related_name = 'records', on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
+    
+    UniqueConstraint(fields=['habit', 'date'], name='unique_habit ') ]
